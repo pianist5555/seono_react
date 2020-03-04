9ea55c62e321ee1fbf3921134664869c79b2d5fb
Seono <pianist5555@gmail.com>
2020-03-05 08:57:01 +0900

    Update home2.js

--- src/components/home2.js
+++ src/components/home2.js
@@ -11,13 +11,17 @@ class Home2 extends Component {
   render() {
     // 오브젝트 비구조화 할당
     const { number, gel } = this.state;
-    console.log(number)
+    console.log(number);
     return (
       <div>
-        <h1>{number} {gel}</h1>
+        <h1>
+          {number} {gel}
+        </h1>
         <button
           onClick={() => {
-            this.setState({ number: number + 1 });
+            this.setState({ number: number + 1 }, () => {
+              console.log("you called me"); // 두번째 파라미터에 콜백함수
+            });
           }}
         >
           +1
