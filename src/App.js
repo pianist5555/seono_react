a493b75765b6ebc9b3313a83160ac961e774bfa5
Seono <pianist5555@gmail.com>
2020-03-04 13:47:37 +0900

    0304

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Home from "./components/home.js";
 
 /*함수형은 state, 라이플사이클 API 사용 불가 대신 hook 사용 페이스북 오픈소스에서 권장*/
 // function App() {
@@ -8,7 +9,7 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return <div>hello</div>;
+    return <Home parameter="react"/>;
   }
 }
 
---
+++ src/components/home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = (props) => {
+  return <div>{props.parameter}</div>;
+};
+
+export default Home;
