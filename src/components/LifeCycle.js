274299aa8ab8360481b87bad09bf73d2f6dbd949
Seono <pianist5555@gmail.com>
2020-03-06 19:08:20 +0900

    0306

---
+++ src/components/LifeCycle.js
@@ -0,0 +1,5 @@
+/*
+Render : componentWillMount() -> render() -> componentDidMount()
+Update : componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+                                                                    *spinner loading 표시를 하는등에 활용할 수 있다.
+*/
--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { DiAndroid } from 'react-icons/di';
+import { IconContext } from "react-icons";
+import { FaReact } from "react-icons/fa";
 
 const Home = props => {
   const { propsName, children } = props;
@@ -9,9 +12,12 @@ const Home = props => {
     // 반면에 자바스크립트에서 사용시 변수명 그대로 사용해도 무관
     //        자바스크립트에서 {}안에 작성하면 객체로 인식
     return (
-      <div>
-        {propsName} {children}
-      </div>
+      <IconContext.Provider value={{ color: "pink", className: "global-class-name",size: "5em" }}>
+        <div>
+          <FaReact />
+          {propsName} {children}
+        </div>
+      </IconContext.Provider>
     );
   }
 };
