5cd7a9747c6ea08c9a171b4a74abe2287dae1622
Seono <pianist5555@gmail.com>
2020-03-06 20:27:53 +0900

    0306

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,23 +1,39 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect} from "react";
 
 const Info = () => {
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
+
+    // useEffect(()=>{
+    //     console.log('렌더링 완료되었습니다! 2번째 인자를 빈 배열로 주었기때문에 최초 마운트될 떄만 실행됩니다.')
+    //     console.log({ 
+    //         name,
+    //         nickname
+    //     });
+    // },[]);
+
+    // 렌더링되면 실행됨 didMount, update(effect에 2번째 인자를 주고 변경이되면)와 비슷하네요
     useEffect(()=>{
-        console.log('렌더링 완료되었습니다!')
+        //name이 바뀔때만 useEffect를 실행시키게 함 -> 2번째 인자가 name을 가르키고 있기에
+        console.log("새로운내용이 변경되었다 name의")
         console.log({
             name,
             nickname
         });
-    });
+    },[name]);
     const onChangeName = e =>{
         setName(e.target.value);
     };
     const onChangeNickName = e =>{
-        setNickName(e.target.value);
+        setNickname(e.target.value);
     };
     
-    return("hi");
+    return(
+    <div>
+        <input name={name} onChange={onChangeName}></input>
+        <input name={nickname} onChange={onChangeNickName}></input>
+    </div>)
+    ;
 };
 
 export default Info;
